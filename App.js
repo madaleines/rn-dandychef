6b7ca7222ea118e2e50b457fb74842551b3382b9
Madaleine Shields <maddiev.shields@gmail.com>
2019-01-24 01:11:03 -0800

    Disable console warnings for demo

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import AppNavigator from './AppNavigator';
 
 export default class App extends React.Component {
   render() {
+    console.disableYellowBox = true;
     return (
       <AppNavigator/>
     );
